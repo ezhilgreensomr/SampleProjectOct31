38e3503d3fb9445a844b186f6864284f92076473
ezhilgreensomr <147533004+ezhilgreensomr@users.noreply.github.com>
2023-11-02 09:12:32 +0530

    Update Student.java

--- src/com/sample/Student.java
+++ src/com/sample/Student.java
@@ -5,8 +5,6 @@ public class Student {
 	public static void main(String[] args) {
 
 		System.out.println("0");
-                                        System.out.println("1");
-                                        System.out.println("2");
 		
 	}
 }
